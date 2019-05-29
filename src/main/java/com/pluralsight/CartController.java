742758131dd36faf55169aa0a00871e619a1f584
Omer Haglili <omerh@example.com>
2019-05-29 15:49:01 +0300

    Completed the module29.5-take3

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -72,7 +72,7 @@ public class CartController extends HttpServlet {
 
 	}
 
-	private void updatCart(HttpServletRequest request , HttpServletResponse response)
+	private void updateCart(HttpServletRequest request , HttpServletResponse response)
 		throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		int index= Integer.parseInt(request.getParameter("index"));
