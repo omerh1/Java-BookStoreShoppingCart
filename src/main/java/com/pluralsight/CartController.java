52b9c343bb2fd784da722f9da9d821ada1077bc7
Omer Haglili <omerh@example.com>
2019-05-29 15:17:46 +0300

    Completed the module29.5-take2

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -44,7 +44,10 @@ public class CartController extends HttpServlet {
 			switch(action) {
 				case "/addcart":
 					 addToCart(request, response);
-           break;
+					 break;
+				case "/delete":
+					deleteFromCart(request, response);
+            		break;
         default:
            break;
 			}
@@ -54,6 +57,16 @@ public class CartController extends HttpServlet {
 		}
 
 		response.sendRedirect("../ShoppingCart.jsp");
+	}
+
+	private void deleteFromCart(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int index= Integer.parseInt(request.getParameter("index"));
+		ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+		cart.deleteCartItem(index);
+
+
 	}
 
   protected void addToCart(HttpServletRequest request, HttpServletResponse response)
